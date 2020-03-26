620e0b6bf8a25e3f5368926ac0563ad3276068fe
moukkari <ilmari.tyrkko@tuni.fi>
2020-03-26 10:45:18 +0200

    First layout with updated shopping cart
    - Ostoskori eritelty storesta
    - Ostoskoriin lisätty Lisää/Poista 1 -nappulat
    - Paljon pieniä viilauksia ja virheentarkistuksia

--- fullstack/frontend/src/App.js
+++ fullstack/frontend/src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-import Store from './Store'
+import ShoppingCart from './ShoppingCart'
 import Admin from './Admin'
 import { Route, BrowserRouter, NavLink } from 'react-router-dom'
+import Store from './Store';
 
 
 // Add Navigation here
 const Navi = () => {
     return (
       <nav className="navbar navbar-expand-sm bg-light navbar-light">
-        <a className="navbar-brand" href="/#">Apple Fanboy Store</a>
+        <a className="navbar-brand" href="/#">KTVO</a>
         <ul className="navbar-nav">
           <li className="nav-item">
-            <NavLink className="nav-item nav-link" to="/store">Store</NavLink>
+            <NavLink className="nav-item nav-link" to="/store">Kauppa</NavLink>
           </li>
           <li className="nav-item">
             <NavLink className="nav-item nav-link" to="/admin">Admin</NavLink>
+          </li>
+          
+        </ul>
+        <ul className="navbar-nav ml-auto">
+          <li>
+            <NavLink className="nav-item nav-link" to="/cart">Ostoskori</NavLink>
           </li>
         </ul>
       </nav>
       )
   }
-
 
 class App extends Component {
     render() {
@@ -28,9 +34,10 @@ class App extends Component {
                   <BrowserRouter>
                   <div>
                       <Navi/>              
-                      <Route exact path="/" component={Store} />
+                      <Route exact path="/" component={ShoppingCart} />
                       <Route exact path="/store" component={Store} />
                       <Route exact path="/admin" component={Admin} />
+                      <Route exact path="/cart" component={ShoppingCart} />
                     </div>
                   </BrowserRouter>
                 </div>
