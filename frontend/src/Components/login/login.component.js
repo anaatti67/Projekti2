235bb71dcd130f1a126a7047924296283715c086
anaatti67 <51318599+anaatti67@users.noreply.github.com>
2020-03-26 13:40:42 +0200

    Kirjautuminen rekisteröityminen

---
+++ frontend/src/Components/login/login.component.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import classes from '../authentication.module.css'
+
+class Login extends Component {
+  render () {
+    return (
+      <form className={classes.form}>
+        <h3>Kirjaudu sisään</h3>
+
+        <div className="">
+          <label>Sähköposti: </label>
+          <input type="email" className="" placeholder="PenttiMalaste@gmail.com" />
+        </div>
+
+        <div className="">
+          <label>Salasana: </label>
+          <input type="password" className="" placeholder="Syötä salasana" />
+        </div>
+
+        <div className="">
+          <div className="">
+            <input type="checkbox" className="" id="customCheck1" />
+
+            <label className="" /* Muistakaa minut voi toteuttaa jos jaksaa */ htmlFor="customCheck1">Muista minut</label>
+          </div>
+        </div>
+
+        <button type="submit" className="">Lähetä</button>
+        <p className="">
+            Unohtuiko <a href="#">Salasana?</a>
+        </p>
+      </form>
+    )
+  }
+}
+
+export default Login
