bbb60c08b417dfe6cbb373d4e65edfeed3538f3e
moukkari <ilmari.tyrkko@tuni.fi>
2020-05-09 13:05:06 +0300

    ReviewModal + other

---
+++ fullstack/frontend/src/ReviewModal.js
@@ -0,0 +1,200 @@
+import React, { Component } from 'react'
+import { Button, Container, Row, Col } from 'react-bootstrap'
+import './css/Modal.css'
+import { NavLink } from 'react-router-dom'
+import starimg from './img/star.png'
+import blankstarimg from './img/starblank.png'
+
+const Stars = (props) => {
+    let starCount = props.stars
+    const star = <img width="25px" alt={starCount} src={starimg} />
+    const blank = <img width="25px" alt='' src={blankstarimg} />
+    let stars = []
+    for (let x = 0; x < 5; x++) {
+        if (starCount > 0) {
+            stars.push(<span key={x}>{star}</span>)
+            starCount--
+        } else {
+            stars.push(<span key={x}>{blank}</span>)
+        }
+    }
+
+    return (
+        <div>{stars}</div>
+    )
+}
+
+export default class ReviewModal extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { show: false, reviews: [], loggedIn: false, 
+            user: { username: 'default' } }
+        this.toggle = this.toggle.bind(this)
+        this.whenFormChanges = this.whenFormChanges.bind(this)
+        this.obj = props.obj
+        this.newReview = { ProductId: this.obj.id, CustomerId: '', CustomerName: '', ReviewTitle: '', ReviewTxt: '', Rating: 5  }
+    }
+    toggle() {
+        if ("user" in localStorage) {
+            console.log('user found in localstorage')
+
+            let retrievedData = JSON.parse(localStorage.getItem("user"))
+            console.log(retrievedData)
+
+            this.newReview = { 
+                ProductId: this.obj.id, 
+                CustomerId: 'BO0XSMyXvVb9cPwWBGeHyI4Zk7o2', 
+                CustomerName: retrievedData.username, 
+                ReviewTitle: '', 
+                ReviewTxt: '', 
+                Rating: 5  }
+
+            this.setState({loggedIn: true, user: retrievedData})
+        } 
+        this.setState({ show: !this.state.show })
+        if (!this.state.show) {
+            fetch('https://ktvo.herokuapp.com/getReview/' + this.obj.id)
+                .then(response => response.json()).then(reviews => {
+                    this.setState({ reviews: reviews })
+                })
+        }
+    }
+    componentDidMount() {
+        fetch('https://ktvo.herokuapp.com/getAverage/' + this.obj.id)
+            .then(response => response.json()).then(avg => {
+                this.obj.Rating = avg[0].Average
+            })
+    }
+    whenFormChanges(event) {
+        if (event.target.name === 'reviewTitle') {
+            this.newReview.ReviewTitle = event.target.value
+        } else if (event.target.name === 'reviewTxt') {
+            this.newReview.ReviewTxt = event.target.value
+        } else if (event.target.name === 'rating') {
+            this.newReview.Rating = parseInt(event.target.value)
+        }
+        console.log(this.newReview)
+    }
+    sendReview() {
+        console.log(this.newReview)
+        const configuration = {
+            method: 'post',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify(this.newReview)
+        }
+        fetch('https://ktvo.herokuapp.com/addReview/', configuration)
+            .then(r => r.json()).then((response) => {
+                console.log(response)
+            })
+    }
+    render() {
+        let reviews = <tr><td colSpan="5">Ei vielä arvosteluita</td></tr>
+        if (this.state.reviews.length > 0) {
+            reviews = this.state.reviews.map((review) => 
+            <Container key={review.ReviewId} style={{background: 'darkslategrey', color: 'white', margin: '3em 0em', padding: '1em', borderRadius: '25px'}}>
+                <Row>
+                    <Col>
+                        <span style={{float: 'right'}}><Stars stars={review.Rating} /></span><h4>{review.ReviewTitle}</h4>
+                    </Col>
+                    <Col><p>Arvostelijan id: {review.CustomerId}<br/>
+                        Arvostelijan nimi: {review.CustomerName}</p></Col>
+                </Row>
+                <Row>
+                    <Col><p>{review.ReviewTxt}</p></Col>
+                </Row>
+            </Container>
+        )}
+        let addReview = <Row><Col><NavLink to="/login" >Kirjaudu sisään</NavLink> lisätäksesi arvostelun.</Col></Row>
+        if (this.state.loggedIn) {
+            addReview = (
+                <Row>
+                    <Col>
+                        {this.state.showAddReview ?
+                        <span>
+                        <Button onClick={() => this.setState({ showAddReview: false })} >Piilota</Button>
+                        <h3>Lisää arvostelu</h3>
+                        <form>
+                            <div className="form-group">
+                                
+                                <p>Käyttäjänimi: {this.state.user.username}</p>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="reviewTitle">Arvostelun otsikko</label>
+                                <input type="text" onChange={this.whenFormChanges} 
+                                    defaultValue={this.newReview.title} name="reviewTitle" 
+                                    className="form-control" id="reviewTitle" placeholder="Otsikko"/>
+                            </div>
+                            
+                            <div className="form-group">
+                                <label htmlFor="reviewTxt">Arvostelu (max. 1000 merkkiä)</label>
+                                <textarea type="text" rows="6" onChange={this.whenFormChanges} 
+                                    defaultValue={this.newReview.txt} name="reviewTxt" 
+                                    className="form-control" id="reviewTxt" placeholder="Kirjoita arvostelu tähän..."/>
+                            </div>
+            
+                            <div className="form-check form-check-inline">
+                                <input onChange={this.whenFormChanges} className="form-check-input" type="radio" name="rating" id="rating5" value="5" defaultChecked={true} />
+                                <label className="form-check-label" htmlFor="rating5">5</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                                <input onChange={this.whenFormChanges} className="form-check-input" type="radio" name="rating" id="rating4" value="4" />
+                                <label className="form-check-label" htmlFor="rating4">4</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                                <input onChange={this.whenFormChanges} className="form-check-input" type="radio" name="rating" id="rating3" value="3" />
+                                <label className="form-check-label" htmlFor="rating3">3</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                                <input onChange={this.whenFormChanges} className="form-check-input" type="radio" name="rating" id="rating2" value="2" />
+                                <label className="form-check-label" htmlFor="rating2">2</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                                <input onChange={this.whenFormChanges} className="form-check-input" type="radio" name="rating" id="rating1" value="1" />
+                                <label className="form-check-label" htmlFor="rating1">1</label>
+                            </div>
+                            <br/>
+                            <button type="button" className="btn btn-success" onClick={() => this.sendReview()}>Lähetä</button>
+                        </form></span>
+                        : <Button onClick={() => this.setState({ showAddReview: true })} >Lisää arvostelu</Button>}
+                    </Col>
+                </Row>
+            )
+        }
+        const style = {
+            background: "rgba(255, 255, 255, 0.9)", 
+            zIndex: "10",
+            display: "inline-block",
+            position: "fixed",
+            top: "0",
+            left: "0",
+            bottom: "0",
+            right: "0",
+            width: "70%",
+            height: "100%",
+            margin: "auto",
+            borderRadius: "25px"
+            }
+        if (this.state.show === true ) {
+            return(
+                <span>
+                <Button>Näytä arvostelut</Button>
+                <div style={style}>
+                <Container className="border pad overflow">
+                    <Row>
+                        <Col>
+                            <Button className="btn btn-danger" style={{float: "right"}} onClick={this.toggle}>X</Button>
+                            <h1>{this.obj.id} - {this.obj.Name} <Stars stars={this.obj.Rating} /></h1>
+                            {addReview}
+                            <hr/>
+                            {reviews}
+                        </Col>
+                    </Row>
+                </Container>
+                </div>
+                </span>
+            )
+        } else {
+            return <span><Stars stars={this.obj.Rating} /><br/><p className="fakelink" onClick={this.toggle}>Näytä arvostelut</p></span>
+        }
+    }
+}
--- fullstack/frontend/src/auth/signup.js
+++ fullstack/frontend/src/auth/signup.js
@@ -7,46 +7,116 @@ class SignIn extends Component {
     // this.login = this.login.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.signup = this.signup.bind(this);
+    this.validationInfo = '';
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      confirmpassword: '',
+      firstname: '',
+      lastname: '',
+      street: '',
+      postal: null,
+      city: '',
+      phone: ''
     };
   }
 
   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
-
+  test() {
+    console.log(this.state)
+    if (this.state.password === this.state.confirmpassword) {
+        console.log('ok')
+    } else {
+        console.log('nope')
+    }
+  }
   signup(e){
-    e.preventDefault();
-    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
-      console.log(u)
-      fire.database().ref('users/' + u.user.uid).set({
-        username: 'Antti on lurjus',  
-        address: this.state.address
+    if (this.state.password === this.state.confirmpassword) {
+       e.preventDefault();
+      fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+        console.log(u)
+        fire.database().ref('users/' + u.user.uid).set({
+          firstname: this.state.firstname,
+          lastname: this.state.lastname,  
+          street: this.state.street,
+          postal: this.state.postal,
+          city: this.state.city,
+          phone: this.state.phone
+        })
+      }).catch((error) => {
+          console.log(error);
       })
-    }).catch((error) => {
-        console.log(error);
-      })
+    } else {
+      this.validationInfo = 'Salasanat eivät täsmää'
+    }
+   
   }
   render() {
     return (
-       <div className="col-md-6">
-       <form>
-      <div className="form-group">
-       <label for="exampleInputEmail1">Email address</label>
-       <input value={this.state.email} onChange={this.handleChange} type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-       <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-      </div>
-       <div className="form-group">
-      <label for="exampleInputPassword1">Password</label>
-      <input value={this.state.password} onChange={this.handleChange} type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-      </div>
-      <button type="submit" onClick={this.login} class="btn btn-primary">Login</button>
-      <button onClick={this.signup} style={{marginLeft: '25px'}} className="btn btn-success">Signup</button>
- </form>
+      <div className="col-md-6">
+        <form>
+          <div className="form-group">
+            <label htmlFor="emailinput">Sähköpostiosoite</label>
+            <input value={this.state.email} onChange={this.handleChange} 
+              type="email" name="email" className="form-control" 
+              id="emailinput" aria-describedby="emailHelp" placeholder="Syötä sähköposti" />
+            <small id="emailHelp" className="form-text text-muted">Emme ikinä jaa sähköpostiosoitettasi muille.</small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="passwordinput">Salasana</label>
+            <input value={this.state.password} onChange={this.handleChange} 
+              type="password" name="password" className="form-control" 
+              id="passwordinput" placeholder="Salasana" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="confirmpasswordinput">Salasana</label>
+            <input value={this.state.confirmpassword} onChange={this.handleChange} 
+              type="password" name="confirmpassword" className="form-control" 
+              id="confirmpasswordinput" placeholder="Vahvista salasana" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="firstinput">Etunimi</label>
+            <input value={this.state.firstname} onChange={this.handleChange} 
+              type="text" name="firstname" className="form-control" 
+              id="firstinput" placeholder="Etunimi" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lastinput">Sukunimi</label>
+            <input value={this.state.password} onChange={this.handleChange} 
+              type="text" name="lastname" className="form-control" 
+              id="lastinput" placeholder="Sukunimi" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="streetinput">Katuosoite</label>
+            <input value={this.state.password} onChange={this.handleChange} 
+              type="text" name="street" className="form-control" 
+              id="streetinput" placeholder="Katuosoite" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="postal">Postinumero</label>
+            <input value={this.state.password} onChange={this.handleChange} 
+              type="number" name="postal" className="form-control" 
+              id="postal" placeholder="Postinumero" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="city">Kaupunki</label>
+            <input value={this.state.password} onChange={this.handleChange} 
+              type="text" name="city" className="form-control" 
+              id="city" placeholder="Kaupunki" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="phone">Puhelinnumero</label>
+            <input value={this.state.password} onChange={this.handleChange} 
+              type="text" name="phone" className="form-control" 
+              id="phone" placeholder="Puhelinnumero" />
+          </div>
+        <p>{this.validationInfo}</p>
+        <button onClick={() => console.log('click')} className="btn btn-success">Signup</button>
+        </form>
  
- </div>
+      </div>
     );
   }
 }
