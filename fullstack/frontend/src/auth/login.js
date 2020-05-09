6adc37cf60ee75d919665a43c2f3c99144af0483
moukkari <ilmari.tyrkko@tuni.fi>
2020-05-09 15:39:36 +0300

    Poistettu Signup-metodi ja nappi

--- fullstack/frontend/src/auth/login.js
+++ fullstack/frontend/src/auth/login.js
@@ -6,7 +6,6 @@ class Login extends Component {
     super(props);
     this.login = this.login.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.signup = this.signup.bind(this);
     this.state = {
       email: '',
       password: ''
@@ -26,19 +25,6 @@ class Login extends Component {
       });
   }
 
-  signup(e){
-    e.preventDefault();
-    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
-      console.log(u)
-      fire.database().ref('users/' + u.user.uid).set({
-        username: 'Antti on lurjus'
-      })
-    }).catch((error) => {
-        console.log(error);
-      })
-  }
-  
-
   render() {
     return (
        <div className="col-md-6">
@@ -53,7 +39,6 @@ class Login extends Component {
       <input value={this.state.password} onChange={this.handleChange} type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
       </div>
       <button type="submit" onClick={this.login} className="btn btn-primary">Login</button>
-      <button onClick={this.signup} style={{marginLeft: '25px'}} className="btn btn-success">Signup</button>
  </form>
  
  </div>
